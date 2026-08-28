8bf5977afccf566f1aa6c859a45da55e60ce9f4d
fhzmx2-blip <fhzmx2@gmail.com>
2026-08-28 12:09:38 +0900

    kafka -ing

---
+++ src/main/java/com/sist/web/kafka/NoticeProducer.java
@@ -0,0 +1,11 @@
+package com.sist.web.kafka;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor   
+public class NoticeProducer {
+
+}
